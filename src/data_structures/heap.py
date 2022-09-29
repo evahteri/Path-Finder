c418d749cc3f3701503140df165c01899254f92d
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-09-29 13:09:45 +0300

    new coverage report, cleaned code

--- src/data_structures/heap.py
+++ src/data_structures/heap.py
@@ -1,25 +1,24 @@
 
 
-
-
 class Heap:
     """This is a min heap, and the cost of the nodes is the deciding factor when building it.
     If a node is in position k, it's left child is in position 2k, right child in position 2k+1,
     and parent in position k//2.
     """
+
     def __init__(self):
         """Constructor that establishes the list that is treated like a heap.
         """
         self._heap = [None]
-    
+
     def get_heap_len(self):
         """Function to get the size of the heap
 
         Returns:
             int: heap size
         """
-        return len(self._heap) -1
-        
+        return len(self._heap) - 1
+
     def _swap(self, pos1, pos2):
         """Swapping function to change two nodes' positions
 
@@ -27,7 +26,8 @@ class Heap:
             pos1 (int): position of the first node
             pos2 (int): position of the second node
         """
-        (self._heap[pos1], self._heap[pos2]) = (self._heap[pos2], self._heap[pos1])
+        (self._heap[pos1], self._heap[pos2]) = (
+            self._heap[pos2], self._heap[pos1])
 
     def push(self, node):
         """Function to insert nodes to the heap
@@ -39,15 +39,13 @@ class Heap:
         """
         self._heap.append(node)
         position = len(self._heap) - 1
-        #Looping until found the right position
+        # Looping until found the right position
         parent_position = position // 2
         if len(self._heap) - 1 > 2:
             while self._heap[position][0] < self._heap[parent_position][0]:
                 self._swap(position, parent_position)
                 position = position // 2
 
-
-    
     def pop_smallest(self):
         """Function to return minimun node from heap. Min node is in position 1.
         It will adjust the binary tree after every pop.
@@ -67,7 +65,7 @@ class Heap:
             smallest_child_position = left__child_position
             right_child_position = left__child_position + 1
             # Check if right child exists
-            if right_child_position <= self.get_heap_len(): 
+            if right_child_position <= self.get_heap_len():
                 # if yes, check it's size and choose smaller child
                 if self._heap[right_child_position][0] < self._heap[left__child_position][0]:
                     smallest_child_position = right_child_position
@@ -78,6 +76,6 @@ class Heap:
             # if no, swap parent and smallest child
             self._swap(position, smallest_child_position)
             position = smallest_child_position
-            left__child_position = 2* smallest_child_position
+            left__child_position = 2 * smallest_child_position
 
         return min_node
--- src/tests/heap_test.py
+++ src/tests/heap_test.py
@@ -9,20 +9,19 @@ class TestHeap(unittest.TestCase):
         self.heap = Heap()
 
     def test_push(self):
-        self.heap.push([0, (1,1)])
+        self.heap.push([0, (1, 1)])
         heap_size = self.heap.get_heap_len()
         self.assertEqual(heap_size, 1)
-    
+
     def test_pop_returns_smallest(self):
-        self.heap.push([0, (1,1)])
-        self.heap.push([2, (2,3)])
+        self.heap.push([0, (1, 1)])
+        self.heap.push([2, (2, 3)])
         smallest = self.heap.pop_smallest()
-        self.assertEqual(smallest, [0, (1,1)])
-    
+        self.assertEqual(smallest, [0, (1, 1)])
+
     def test_pop_removes_element(self):
-        self.heap.push([0, (1,1)])
-        self.heap.push([2, (2,3)])
+        self.heap.push([0, (1, 1)])
+        self.heap.push([2, (2, 3)])
         self.heap.pop_smallest()
         heap_size = self.heap.get_heap_len()
         self.assertEqual(heap_size, 1)
-    
