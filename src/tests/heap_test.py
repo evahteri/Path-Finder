f46a2aad88ad4e7455a7adfcb19adefcbe543d7f
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-09-29 11:54:42 +0300

    tests for heap created

---
+++ src/tests/heap_test.py
@@ -0,0 +1,28 @@
+
+import unittest
+from data_structures.heap import Heap
+
+
+class TestHeap(unittest.TestCase):
+
+    def setUp(self):
+        self.heap = Heap()
+
+    def test_push(self):
+        self.heap.push([0, (1,1)])
+        heap_size = self.heap.get_heap_len()
+        self.assertEqual(heap_size, 1)
+    
+    def test_pop_returns_smallest(self):
+        self.heap.push([0, (1,1)])
+        self.heap.push([2, (2,3)])
+        smallest = self.heap.pop_smallest()
+        self.assertEqual(smallest, [0, (1,1)])
+    
+    def test_pop_removes_element(self):
+        self.heap.push([0, (1,1)])
+        self.heap.push([2, (2,3)])
+        self.heap.pop_smallest()
+        heap_size = self.heap.get_heap_len()
+        self.assertEqual(heap_size, 1)
+    
