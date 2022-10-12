2e9320f2c40a6b9233f71096554727a98c1fcdbc
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-10-12 15:05:36 +0300

    feature to change between maps via gui

---
+++ documentation/weekly_reports/week6.md
@@ -0,0 +1,16 @@
+# Week 4
+
+## Plan
+- Improve some of the things that was mentioned in peer review
+- Continue performance testing
+- More tests to make sure the right path is found
+- Trying other, different sized maps
+
+## Progress
+- GUI now allows to change between maps and shows them
+
+## Learning progress
+
+
+
+## Problems/questions
--- src/ui/start_view.py
+++ src/ui/start_view.py
@@ -1,4 +1,5 @@
-from tkinter import constants, ttk, Canvas
+import os
+from tkinter import constants, ttk, Canvas, StringVar
 from algorithms.ida_star import IdaStar
 from algorithms.dijkstra import Dijkstra
 
@@ -11,10 +12,13 @@ class StartViewUi:
     def __init__(self, root):
         self._root = root
         self._frame = None
-        self.grid = Canvas(self._root)
+        self.grid = Canvas(self._root, width=500, height=500)
         self.start_coordinate = None
         self.end_coordinate = None
-        self._grid()
+        self.current_map = None
+        self._pixel_size = None
+        self.map_size = 0
+        self.maps = []
 
         self._base()
 
@@ -49,13 +53,22 @@ class StartViewUi:
         end_coordinate_header.grid(row=2, column=0)
         self.end_coordinate_entry.grid(row=2, column=1)
 
-        header = ttk.Label(master=self._frame, text="Select algorithm")
-        header.grid(row=4, column=0)
+        map_header = ttk.Label(master=self._frame, text="Select map")
+        for file in os.listdir("src/static/maps"):
+            self.maps.append(file)
+        map_header.grid(row=4, column=0)
+
+        self.current_map = StringVar(master=self._frame)
+        map_selection = ttk.OptionMenu(self._frame, self.current_map, self.maps[0], *self.maps)
+        map_selection.grid(row=5, column=0)
+
+        algo_header = ttk.Label(master=self._frame, text="Select algorithm")
+        algo_header.grid(row=7, column=0)
 
         ida_star_button = ttk.Button(
             master=self._frame, text="IDA*", command=self._handle_ida_star
         )
-        ida_star_button.grid(row=5, column=0)
+        ida_star_button.grid(row=8, column=0)
 
         dijkstra_button = ttk.Button(
             master=self._frame, text="Dijkstra", command=self._handle_dijkstra
@@ -63,19 +76,30 @@ class StartViewUi:
         show_map_button = ttk.Button(
             master=self._frame, text="Show map", command=self._grid
         )
-        dijkstra_button.grid(row=6, column=0)
-        show_map_button.grid(row=7, column=0)
+        dijkstra_button.grid(row=9, column=0)
+        show_map_button.grid(row=10, column=0)
+        
+    def _get_map_size(self):
+        size = 0
+        map = self.current_map.get()
+        with open(f"src/static/maps/{map}") as current_map:
+            for row in current_map:
+                size += 1
+        return size
 
     def _grid(self):
-        size = 20
-        with open("../Path_Finder/src/static/maps/map_1.txt") as current_map:
+        self.grid.delete("all")
+        map = self.current_map.get()
+        self.map_size= self._get_map_size()
+        self._pixel_size = 300 // self.map_size
+        with open(f"src/static/maps/{map}") as current_map:
             y_coordinate = 0
             x_coordinate = 0
             for row in current_map:
                 y_coordinate += 1
                 x_coordinate = 0
                 for coordinate in row:
-                    if x_coordinate == 10:
+                    if x_coordinate == self.map_size:
                         break
                     if coordinate == ".":
                         color = "white"
@@ -83,10 +107,10 @@ class StartViewUi:
                         color = "black"
                     if coordinate == "x":
                         color = "red"
-                    x1 = x_coordinate*size
-                    y1 = y_coordinate*size
-                    x2 = x1 + size
-                    y2 = y1 + size
+                    x1 = x_coordinate*self._pixel_size
+                    y1 = y_coordinate*self._pixel_size
+                    x2 = x1 + self._pixel_size
+                    y2 = y1 + self._pixel_size
                     self.grid.create_rectangle((x1, y1, x2, y2), fill=color)
                     x_coordinate += 1
         self.grid.pack()
