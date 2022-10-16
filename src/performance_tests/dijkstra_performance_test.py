2ad435f910d923e55f927c3801599e04f0a6d5f6
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-10-16 11:17:18 +0300

    new map and new performance tests

--- src/performance_tests/dijkstra_performance_test.py
+++ src/performance_tests/dijkstra_performance_test.py
@@ -31,10 +31,10 @@ class Dijkstra_Performance():
         """
         start_time = datetime.datetime.now()
         for i in range(100):
-            start_x = randint(0, 9)
-            start_y = randint(0, 9)
-            goal_x = randint(0, 9)
-            goal_y = randint(0, 9)
+            start_x = randint(0, 14)
+            start_y = randint(0, 14)
+            goal_x = randint(0, 14)
+            goal_y = randint(0, 14)
             if InputCheck().check_input(current_map="map_2.txt", x_start=start_x, y_start=start_y, x_end=goal_x, y_end=goal_y):
                 Dijkstra("map_2.txt").find_route(
                     start_x, start_y, goal_x, goal_y)
@@ -46,12 +46,27 @@ class Dijkstra_Performance():
         """
         start_time = datetime.datetime.now()
         for i in range(100):
-            start_x = randint(0, 9)
-            start_y = randint(0, 9)
-            goal_x = randint(0, 9)
-            goal_y = randint(0, 9)
+            start_x = randint(0, 29)
+            start_y = randint(0, 29)
+            goal_x = randint(0, 29)
+            goal_y = randint(0, 29)
             if InputCheck().check_input(current_map="map_4.txt", x_start=start_x, y_start=start_y, x_end=goal_x, y_end=goal_y):
                 Dijkstra("map_4.txt").find_route(
+                    start_x, start_y, goal_x, goal_y)
+        finish_time = datetime.datetime.now()
+        return finish_time-start_time
+    
+    def test_dijkstra_10_times_50x50_map(self):
+        """Paths are fetched 10 times in 50x50 sized map (map_3.txt)
+        """
+        start_time = datetime.datetime.now()
+        for i in range(10):
+            start_x = randint(0, 49)
+            start_y = randint(0, 49)
+            goal_x = randint(0, 49)
+            goal_y = randint(0, 49)
+            if InputCheck().check_input(current_map="map_3.txt", x_start=start_x, y_start=start_y, x_end=goal_x, y_end=goal_y):
+                Dijkstra("map_3.txt").find_route(
                     start_x, start_y, goal_x, goal_y)
         finish_time = datetime.datetime.now()
         return finish_time-start_time
--- src/performance_tests/performance.py
+++ src/performance_tests/performance.py
@@ -72,3 +72,24 @@ class PerformanceTest():
             print(
                 f"Dijkstra star was faster by {difference_time.microseconds} microseconds, {difference_percentage}")
             return ("Dijkstra", difference_time.microseconds, difference_percentage)
+
+    def test_performance_50x50_map(self):
+        dijkstra_time = Dijkstra_Performance().test_dijkstra_10_times_50x50_map()
+        ida_star_time = IdaStar_Performance().test_ida_star_10_times_50x50_map()
+        print(
+            f"Dijkstra found 100 routes in 50x50 map in {dijkstra_time.microseconds} microseconds")
+        print(f"IDA* found 100 routes in 50x50 map in {ida_star_time.microseconds} microseconds")
+        difference_time = abs(dijkstra_time - ida_star_time)
+        if dijkstra_time > ida_star_time:
+            difference_percentage = round(
+                (difference_time / dijkstra_time * 100), 2)
+            print(
+                f"IDA* star was faster by {difference_time.microseconds} microseconds, {difference_percentage} %")
+            return ("IDA*", difference_time.microseconds, difference_percentage)
+        else:
+            difference_percentage = round(
+                (difference_time / ida_star_time * 100), 2)
+            print(
+                f"Dijkstra star was faster by {difference_time.microseconds} microseconds, {difference_percentage}")
+            return ("Dijkstra", difference_time.microseconds, difference_percentage)
+
